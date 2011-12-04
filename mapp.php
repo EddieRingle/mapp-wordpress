de5038dfacde98ab5c2a216562b0c4fd6f6053a3
Eddie Ringle <eddie@eringle.net>
2011-12-03 22:32:27 -0500

    Add initial plugin php file
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

---
+++ mapp.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: MAPP for WordPress
+Plugin URI: http://github.com/lockergnome/mapp-wordpress
+Description: Multi-Author Payment Plugin for WordPress
+Version: 0.1-alpha1
+Author: Eddie Ringle
+Author URI: http://eddieringle.com
+License: New BSD
+*/
+?>
